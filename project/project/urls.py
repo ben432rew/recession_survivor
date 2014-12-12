7e5899566d31cec1c9365cd89d5a2c28a9b87cdb
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-12 15:54:03 -0500

    setting up urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^game/', include('blog.urls', namespace = 'game')),
+    url(r'^users/', include('users.urls', namespace = 'users')),
+    url(r'^portfolio/', include('portfolio.urls', namespace = 'portfolio')),
 )
