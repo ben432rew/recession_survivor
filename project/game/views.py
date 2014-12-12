788b4fe8468dea86e35be92e8c4f11b618b9827f
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-12 15:37:26 -0500

    redme

--- project/game/views.py
+++ project/game/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.models import User
 
-# Create your views here.
+class Index(View):
+    def get(self, request):
+        
