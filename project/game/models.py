ffe1274a0e44b56d02cddfa486ca283beceb9510
jackson- <jackson.t.devin@gmail.com>
2014-12-13 02:14:27 -0500

    seeded database.....finally

--- project/game/models.py
+++ project/game/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+	price = models.IntegerField()
+	date = models.DateField()
+	# ticker = models.CharField(max_length=50)
