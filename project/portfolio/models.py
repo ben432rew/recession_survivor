303ee8df16e1ec4411381d6d9a1d44207ca35665
adolfo0620 <adolfo0620@gmail.com>
2014-12-13 00:50:31 -0500

    model works

--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -4,31 +4,31 @@ from django.db import models
 class Portfolio(models.Model):
 	final_score = models.FloatField()
 	date_played = models.DateTimeField(auto_now=True)
-	user = models.ForeignKey('h_trader.User')
+	user = models.ForeignKey('users.User')
 	balance = models.FloatField()
 
 class Transcation(models.Model):
-	symbol = models.CharField(max_lenght=50)
+	symbol = models.CharField(max_length=50)
 	number_of_shares = models.IntegerField()
 	date_created = models.DateTimeField(auto_now_add=True)
 	account_change = models.FloatField()
-	portfolio = models.ForeignKey('h_trader.Portfolio')
+	portfolio = models.ForeignKey('Portfolio')
 
 class Stock_owned(models.Model):
-	symbol = models.CharField(max_lenght=50)
+	symbol = models.CharField(max_length=50)
 	amount = models.IntegerField()
 	price_bought = models.FloatField()
 	date_bought = models.DateField()
-	name = models.CharField(max_lenght=50)
-	portfolio = models.ForeignKey('h_trader.Portfolio')
+	name = models.CharField(max_length=50)
+	portfolio = models.ForeignKey('Portfolio')
 
 class Stock(models.Model):
-	symbol = models.CharField(max_lenght=50)
+	symbol = models.CharField(max_length=50)
 	price = models.FloatField()
 	date = models.DateField()
-	name = models.CharField(max_lenght=50)
+	name = models.CharField(max_length=50)
 	volume = models.FloatField()
 
 class snippet(models.Model):
-	stock = models.ForeignKey('h_trader.Stock')
+	stock = models.ForeignKey('Stock')
 	snippet = models.TextField()
