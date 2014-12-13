5379e3bf75cb3318842583742266a9f96d593d50
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-12 20:20:18 -0500

    skeleton portfolio view

--- project/game/views.py
+++ project/game/views.py
@@ -3,5 +3,22 @@ from django.views.generic import View
 
 
 class Welcome(View):
+    #check if the user has any unfinished games
     def get(self, request):
         pass
+
+class Start(View):
+    def get(self, request):
+        pass
+
+class Round(View):
+    def get(self, request):
+        pass  
+
+    def post(self, request):
+        pass
+
+class Endgame(View):
+    def get(self, request):
+        pass
+
