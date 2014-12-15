37c84460acd98c4232c2d02cb9e7cbd4bbc7ce8d
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-14 23:02:29 -0500

    display all and stock owned finishing no testing

--- project/game/views.py
+++ project/game/views.py
@@ -1,18 +1,22 @@
 from django.views.generic import View
 from django.shortcuts import render
+from portfolio.models import Stock
 
 
 class Round(View):
+    #first time the round will be called by a GET, every time after will be a POST
+    def get(self, request):
+        #june 2009 and june 2008 exist in the db now, deleting june 2009 for easy querying
+        extras = Stock.objects.filter(date__month=6, date__year=2009).delete()
+        request.session['game_round'] = 0
+        return render(request, 'game/round.html', {'user':request.user})
+
     def post(self, request):
-        if 'game_round' in request.session.keys():
-            if request.session['game_round'] == 12:
-                return redirect('game/endgame.html')
-            else:
-                request.session['game_round'] += 1
-                return render( request, 'game/round.html', {'user':request.user})
+        if request.session['game_round'] == 12:
+            return redirect('game/endgame.html')
         else:
-            request.session['game_round'] = 1
-            return render(request, 'game/round.html', {'user':request.user})
+            request.session['game_round'] += 0
+            return render( request, 'game/round.html', {'user':request.user})
 
 
 class Endgame(View):
--- project/portfolio/views.py
+++ project/portfolio/views.py
@@ -2,38 +2,38 @@ from django.views.generic import View
 from django.shortcuts import render
 from portfolio.models import Portfolio, Stock
 
-class Find_stock_by_name(symbol):
+class Find_stock_by_name(View):
     def get(self, request):
-        owned = Portfolio.objects.select_related('Stock_owned').filter(user=request.user)
         game_round = request.session['game_round']
-        return render( request, 'game/round.html', {"game_round":game_round, "stock_owned":owned}))
+        return render( request, 'game/round.html', {"game_round":game_round, 'user':request.session.user}))
 
 
-class Display_all():
+class Display_all(View):
     def get(self, request):
-        all_stocks = Stock.objects.filter(date=())
-        current = all_stocks
-        return render( request, 'game/round.html', {"current":current}))
-        #show all the stocks there are to possible to buy, paginated
+        game_round = request.session['game_round']        
+        if request.session['game_round'] > 6:
+            all_stocks = Stock.objects.filter(date__month=(request.session['game_round'] - 6))
+        else:
+            all_stocks = Stock.objects.filter(date__month=(request.session['game_round'] + 6))
+        return render( request, 'game/round.html', {"game_round":game_round, "all_stocks":all_stocks, 'user':request.session.user}))
 
 
-class Display_owned(portfolio):
+class Display_owned(View):
     def get(self, request):
-        info = Portfolio.objects.select_related('Stock_owned').filter(user=user_obj)
-        current = info
-        return render( request, 'game/round.html', {"current":current}))
-        #show all the users' current stocks owned
+        game_round = request.session['game_round']
+        owned = Portfolio.objects.select_related('Stock_owned').filter(user=request.user)
+        return render( request, 'game/round.html', {"game_round":game_round, "stock_owned":owned, 'user':request.session.user}))
 
 
-class Buy_stock(portfolio, symbol, shares):
+class Buy_stock(View):
     def post(self, request):
         current = None
-        return render( request, 'game/round.html', {"current":current}))
+        return render( request, 'game/round.html', {"game_round":game_round, "stock_owned":owned, 'user':request.session.user}))
         #buy shares of a stock
 
 
-class Sell_shares(portfolio, symbol, shares):
+class Sell_shares(View):
     def post(self, request):
         current = None
-        return render( request, 'game/round.html', {"current":current}))
+        return render( request, 'game/round.html', {"game_round":game_round, "stock_owned":owned, 'user':request.session.user}))
         #sell shares of a stock
