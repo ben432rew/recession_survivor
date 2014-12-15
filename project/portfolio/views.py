21ac617ab65b396f09b547263c5d584a69f7b9db
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-15 11:22:05 -0500

    fixed bug

--- project/portfolio/views.py
+++ project/portfolio/views.py
@@ -14,7 +14,7 @@ class Find_stock_by_name(View):
 class Display_all(View):
     def get(self, request):
         game_round = request.session['game_round']        
-        all_stocks = Stock.objects.filter(date__month=(date__month=(request.session['game_round'] + 6) % 12)
+        all_stocks = Stock.objects.filter(date__month(request.session['game_round'] + 6) % 12)
         return render( request, 'game/round.html', {"game_round":game_round, "all_stocks":all_stocks, 'user':request.session.user}))
 
 
