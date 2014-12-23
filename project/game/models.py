8e1202c0a63a00c0b4fb30784cf8c46ecaba8039
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-22 16:57:20 -0800

    rearranging models to be less entangled

--- project/game/models.py
+++ project/game/models.py
@@ -1,7 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Stock(models.Model):
-	price = models.IntegerField()
-	date = models.DateField()
-	ticker = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=50)
+    price = models.FloatField()
+    date = models.DateField()
+    name = models.CharField(max_length=50)
+    volume = models.FloatField()
+
+
+class Whole_Game(models.Model):
+    final_score = models.FloatField(default=0)
+    date_played = models.DateField(auto_now=True)
+    user = models.ForeignKey(User)
+    balance = models.FloatField()
+
+
+class Transaction(models.Model):
+    symbol = models.CharField(max_length=50)
+    number_of_shares = models.IntegerField()
+    date_created = models.DateField(auto_now_add=True)
+    account_change = models.FloatField()
+    portfolio = models.ForeignKey('Portfolio')
+
+
+class snippet(models.Model):
+    stock = models.ForeignKey('Stock')
+    snippet = models.TextField()
