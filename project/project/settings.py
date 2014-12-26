6b7c08740dc102709440da4951a114b94b4f0733
william <wmantly@gmail.com>
2014-12-26 15:52:38 -0500

    added middleware and README.md

--- project/project/settings.py
+++ project/project/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ui.middleware.ajax'
 )
 
 ROOT_URLCONF = 'project.urls'
---
+++ project/ui/middleware.py
@@ -0,0 +1,13 @@
+class ajax(object):
+
+    def process_request(self, request):
+
+        request.context_dict = {}
+
+        if request.is_ajax():
+            request.context_dict['base_template'] = "ui/ajax.html"
+
+    # def process_response(self, request, response):
+
+    #     if getattr(request, 'locale', False):
+    #         response.cookies['locale'] = request.locale
