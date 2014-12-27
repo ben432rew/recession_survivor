dd32d0b736c9440d41dc2e7b0bc6e6e9821b5df7
Benjamin Himley <benjaminhimley85@gmail.com>
2014-12-27 08:24:57 -0800

    marqueeing

--- project/users/urls.py
+++ project/users/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^login', Login.as_view() ),
     url(r'^logout', Logout.as_view() ),
     url(r'^welcome', Welcome.as_view() ),
+    url(r'^changepassword', ChangePass.as_view() ),
+    url(r'^highscroes', HighScores.as_view() ),
 )
