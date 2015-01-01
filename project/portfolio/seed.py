6d6650c33bfb19645f8ead999c1011b03d52f0b8
William Mantly <wmantly@gmail.com>
2015-01-01 15:00:19 -0500

    Update seed.py
    #92 fixed

--- project/portfolio/seed.py
+++ project/portfolio/seed.py
@@ -2,7 +2,7 @@ import requests
 from portfolio.models import Stock_history, Stocks_Tracked
 from pprint import pprint
 
-symbols = [ 'KWR','GOOG', 'MSFT', 'FB', 'YHOO', 'ORCL', 'IBM', 'AAPL', 'BBY',
+symbols = [ 'KWR','GOOG', 'GOOGL', 'MSFT', 'FB', 'YHOO', 'ORCL', 'IBM', 'AAPL', 'BBY',
     'AMZN', 'GE', 'XOM', 'CVX', 'C', 'ED', 'CL', 'WSM', 'PG', 'K', 'HSY', 'DIS',
     'COH', 'ALL', 'GIS', 'CAG', 'KO', 'PEP', 'M', 'WHR', 'MKC', 'NKE', 'KWR',
     'SJM', 'WFM', 'AEO', 'RL', 'HD', 'GPS', 'UA', 'CSCO', 'BBBY', 'GM', 'F', 
@@ -12,18 +12,25 @@ symbols = [ 'KWR','GOOG', 'MSFT', 'FB', 'YHOO', 'ORCL', 'IBM', 'AAPL', 'BBY',
 
 def seed():
     for symbol in symbols:
+        print( 'symbol:', symbol, "===========" )
+
+        # todaye = str( datetime.date.today() )
+
+        if Stocks_Tracked.objects.filter( symbol=symbol ).exists():
+            print( "all ready tracked" )
+            continue
+
         response = requests.get("https://www.quandl.com/api/v1/datasets/WIKI/"+ symbol +".json?auth_token=M-1QA4sjxLA99bD7QjbN&trim_start=2008-06-01&trim_end=2014-12-30&collapse=daily")
         response = response.json()
 
+
         if response['errors']:
-            print( 'error:', response.error )
+            print( 'error:', response['errors'])
             continue
 
         if not response['data']:
             print( 'No data' )
             continue
-
-        print( 'symbol:', symbol )
 
         for row in response['data']:
             entry = {}
@@ -37,7 +44,11 @@ def seed():
             entry['dividend'] = row[6]
             entry['split_ratio'] = row[7]
 
-            Stock_history.objects.create( **entry )
+            try:
+                Stock_history.objects.create( **entry )
+            except:
+                print( "??" )
+                pprint( entry )
 
         Stocks_Tracked.objects.create( 
             symbol=symbol,
