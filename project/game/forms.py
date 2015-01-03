76e1e028f4943348f823ead95650e08f34672831
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-03 12:04:30 -0500

    changing current_date name on form

--- project/game/forms.py
+++ project/game/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from game.models import *
 from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 from pprint import pprint as print
 
 class DateInput(forms.DateInput):
@@ -12,4 +13,7 @@ class GameCreateForm( ModelForm ):
         fields = ( 'name', 'balance', 'total_rounds' , 'game_type', 'current_date' )
         widgets = {
             'current_date' : DateInput()
+        }
+        labels = {
+            'current_date': _('Starting Day of Your Game')
         }
