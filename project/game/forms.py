9c73c03f6e47aed3edbde1a7eed66d5d6050e01f
adolfo0620 <adolfo0620@gmail.com>
2015-01-04 15:00:11 -0500

    working on overriding clean method

--- project/game/forms.py
+++ project/game/forms.py
@@ -4,21 +4,22 @@ from django.forms import ModelForm
 from django.utils.translation import ugettext_lazy as _
 from pprint import pprint as print
 from django.core.exceptions import ValidationError
-from datetime import datetime
+import datetime
 
 def val_round(val_round):
     if (val_round<=0):
-        raise ValidationError("Round must be greater 0")
+        raise ValidationError("Round must be greater 0", code='invalid')
 
 def val_bal(bal):
     if(bal<=0):
-        raise ValidationError("Balance must be greater than 0")
-def val_date( date):
-    d = datetime.now()
-    dated = datetime.strptime(date, '%Y-%m-%d').date()
-    if (dated >= d.date()):
-        raise ValidationError("This date must be in the past")
+        raise ValidationError("Balance must be greater than 0",code='invalid')
 
+def val_date( date):
+    print('val_date')
+    d = datetime.datetime.now()
+    dated = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    if (dated >= d.date()):
+        raise ValidationError("This date must be in the past", code='invalid')
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -34,5 +35,26 @@ class GameCreateForm( ModelForm ):
         labels = {
             'current_date': _('Starting Day of Your Game (use format 9/20/2011)')
         }
+
+    def clean(self):
+        data = self.cleaned_data
+        print(self.is_valid())
+        cur_date = datetime.datetime.strptime( data['current_date'], "%Y-%m-%d" ).date()
+        
+        total_round = data['total_rounds']
+        game_type = data['game_type']
+        if game_type == 'weekly':
+            num_days = 7 * total_round
+        elif game_type == 'monthly':
+            num_days = 31 * total_round
+        elif game_type == 'yearly':
+            num_days = 365 * total_round
+
+        check_date = datetime.datetime.now().date() - datetime.timedelta(days=num_days)
+
+        if(check_date <= cur_date):
+            raise forms.ValidationError('Date must be further in the past')
+
+        return self.cleaned_data
 
 
