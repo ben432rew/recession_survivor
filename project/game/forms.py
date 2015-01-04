02560f3d93865a913aa6b8531e1fc5f73bc9fc30
adolfo0620 <adolfo0620@gmail.com>
2015-01-04 17:15:21 -0500

    moved val_date into clean

--- project/game/forms.py
+++ project/game/forms.py
@@ -14,7 +14,8 @@ def val_bal(bal):
     if(bal<=0):
         raise ValidationError("Balance must be greater than 0",code='invalid')
 
-def val_date( date):
+def val_date(date):
+
     print('val_date')
     d = datetime.datetime.now()
     dated = datetime.datetime.strptime(date, '%Y-%m-%d').date()
@@ -26,8 +27,8 @@ class DateInput(forms.DateInput):
 
 class GameCreateForm( ModelForm ):
     balance = forms.DecimalField(validators=[val_bal], initial=10000)
-    total_rounds = forms.IntegerField(validators=[val_round], initial=12)
-    current_date = forms.CharField(validators=[val_date],widget=forms.DateInput(attrs={'type': 'date'}))
+    total_rounds = forms.IntegerField(initial=12)
+    current_date = forms.CharField( widget=forms.DateInput(attrs={'type': 'date'}))
     
     class Meta:
         model = Whole_Game
@@ -36,9 +37,16 @@ class GameCreateForm( ModelForm ):
             'current_date': _('Starting Day of Your Game (use format 9/20/2011)')
         }
 
+
     def clean(self):
+        # print(dir(self))
         data = self.cleaned_data
-        print(self.is_valid())
+        val_round(data['total_rounds'])
+        val_date(data['current_date'])
+        print(data)
+        if 'name' or 'balance' or 'total_rounds' or 'game_type' or 'current_date' not in data:
+            raise forms.ValidationError('PLease enter Valid Values ')
+       
         cur_date = datetime.datetime.strptime( data['current_date'], "%Y-%m-%d" ).date()
         
         total_round = data['total_rounds']
@@ -55,6 +63,6 @@ class GameCreateForm( ModelForm ):
         if(check_date <= cur_date):
             raise forms.ValidationError('Date must be further in the past')
 
-        return self.cleaned_data
+    #     return self.cleaned_data
 
 
