82baca5b85c4a3b1b7d25800be71d504c601b8f2
adolfo0620 <adolfo0620@gmail.com>
2015-01-04 19:28:12 -0500

    fixed a bug and that if left empyt break server and break methods into smaller breaks

--- project/game/forms.py
+++ project/game/forms.py
@@ -6,6 +6,27 @@ from pprint import pprint as print
 from django.core.exceptions import ValidationError
 import datetime
 
+def date_far_enough(data):
+    cur_date = datetime.datetime.strptime( data['current_date'], "%Y-%m-%d" ).date() 
+    total_round = data['total_rounds']
+    game_type = data['game_type']
+
+    num_days = _cal_num_days(game_type, total_round)
+    check_date = datetime.datetime.now().date() - datetime.timedelta(days=num_days)
+
+    if(check_date <= cur_date):
+        raise forms.ValidationError('Date must be further in the past')
+
+def _cal_num_days(game_type, total_round):
+    if game_type == 'weekly':
+        num_days = 7 * total_round
+    elif game_type == 'monthly':
+        num_days = 31 * total_round
+    elif game_type == 'yearly':
+        num_days = 365 * total_round
+    return num_days
+
+
 def val_round(val_round):
     if (val_round<=0):
         raise ValidationError("Round must be greater 0", code='invalid')
@@ -16,7 +37,6 @@ def val_bal(bal):
 
 def val_date(date):
 
-    print('val_date')
     d = datetime.datetime.now()
     dated = datetime.datetime.strptime(date, '%Y-%m-%d').date()
     if (dated >= d.date()):
@@ -25,6 +45,7 @@ def val_date(date):
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+# RegexValidator
 class GameCreateForm( ModelForm ):
     balance = forms.DecimalField(initial=10000)
     total_rounds = forms.IntegerField(initial=12)
@@ -41,28 +62,14 @@ class GameCreateForm( ModelForm ):
         }
 
     def clean(self):
-        # print(dir(self))
+        if (self.is_valid()==False):
+            raise forms.ValidationError('Please enter all fields')
+
         data = self.cleaned_data
         val_round(data['total_rounds'])
         val_date(data['current_date'])
         val_bal(data['balance'])
-        print(data)
-       
-        cur_date = datetime.datetime.strptime( data['current_date'], "%Y-%m-%d" ).date()
+        date_far_enough(data)
+
+        return self.cleaned_data
         
-        total_round = data['total_rounds']
-        game_type = data['game_type']
-        if game_type == 'weekly':
-            num_days = 7 * total_round
-        elif game_type == 'monthly':
-            num_days = 31 * total_round
-        elif game_type == 'yearly':
-            num_days = 365 * total_round
-
-        check_date = datetime.datetime.now().date() - datetime.timedelta(days=num_days)
-
-        if(check_date <= cur_date):
-            raise forms.ValidationError('Date must be further in the past')
-
-
-
