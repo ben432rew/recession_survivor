a6e7b0ff9506d2ced06a4fbf5e4620349031025f
William Mantly <wmantly@gmail.com>
2015-07-28 23:47:30 -0400

    Update usershome.js

--- project/users/static/users/usershome.js
+++ project/users/static/users/usershome.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#change_pass_modal").hide()
+    $("#change_pass_modal").hide();
     $('#change_pass_btn').on('click', function(e) {
         $( "#main" ).hide();
         $("#change_pass_modal").show();
